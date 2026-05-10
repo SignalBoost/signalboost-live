8a7144a34703e26d357fa6e2ff09a71f3ed13b65
SignalBoost <cadomos@gmail.com>
2026-05-10 04:21:05 -0600

    Add Supabase SessionContextProvider to MyApp

--- saas/pages/_app.js
+++ saas/pages/_app.js
@@ -1,1 +1,14 @@
 
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { supabase } from '../lib/supabaseClient';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+}

874a9fafb005cfeeaa579e85822dd2f867ff1a66
SignalBoost <cadomos@gmail.com>
2026-05-10 08:10:14 -0600

    Update Supabase client initialization in _app.js

--- saas/pages/_app.js
+++ saas/pages/_app.js
@@ -1,13 +1,12 @@
-
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { supabase } from '../lib/supabaseClient';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { useState } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
+    <SessionContextProvider supabaseClient={supabaseClient}>
       <Component {...pageProps} />
     </SessionContextProvider>
   );

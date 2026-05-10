10737e43cba51656d8b7a8782b4916ccc4261f5d
SignalBoost <cadomos@gmail.com>
2026-05-10 17:46:32 -0600

    Update _app.js

--- saas/pages/_app.js
+++ saas/pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/globals.css';
+import Link from 'next/link';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header style={{ padding: '20px', background: '#111', color: '#fff' }}>
+        <nav style={{ display: 'flex', gap: '20px' }}>
+          <Link href="/generator">Website Generator</Link>
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/signup">Sign Up</Link>
+          <Link href="/login">Login</Link>
+        </nav>
+      </header>
+
+      <Component {...pageProps} />
+    </>
+  );
 }

ae04001c2d3974390e8b46e1d8d8109699a2ce4d
SignalBoost <cadomos@gmail.com>
2026-05-14 15:25:12 -0600

    Reorder content paths in Tailwind config

--- saas/tailwind.config.js
+++ saas/tailwind.config.js
@@ -2,11 +2,11 @@
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };

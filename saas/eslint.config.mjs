2d03d404b77e1cc88c7dc61483310594760a6d44
SignalBoost <cadomos@gmail.com>
2026-05-13 21:01:30 -0600

    Refactor ESLint configuration export syntax

--- saas/eslint.config.mjs
+++ saas/eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,10 +10,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals"),
-  // Add "next/typescript" if you're using TypeScript heavily
-  // ...compat.extends("next/typescript"),
 ];
-
-export default eslintConfig;

036b7183631c81b0389c6a6dc34683abf359d2b2
SignalBoost <cadomos@gmail.com>
2026-05-14 08:38:45 -0600

    Update next.config.js

--- saas/next.config.js
+++ saas/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Optional: Vercel build ignore patterns
   experimental: {
     serverActions: {
       allowedOrigins: ["*"],

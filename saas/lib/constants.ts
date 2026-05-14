d96478c61f539654060dca211236af88ec81b730
SignalBoost <cadomos@gmail.com>
2026-05-14 13:12:48 -0600

    Delete saas/lib directory

--- saas/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-import type { GenerationMode } from "@/lib/types";
-
-export const CREDIT_COSTS: Record<GenerationMode, number> = {
-  text: 1,
-  audio: 2,
-  video: 5,
-  translate: 1,
-  visual: 3,
-};

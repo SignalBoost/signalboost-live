a8a0198372a44127567611d61708fa933a5d8b3f
SignalBoost <cadomos@gmail.com>
2026-05-15 20:11:05 -0600

    Refactor Page component to Home and update imports

--- saas/app/page.tsx
+++ saas/app/page.tsx
@@ -1,9 +1,11 @@
-import Hero from '../components/Hero'
+import Navbar from '@/components/Navbar'
+import SignalHero from '@/components/SignalHero'
 
-export default function Page() {
+export default function Home() {
   return (
-    <main className="bg-black min-h-screen w-full">
-      <Hero />
+    <main className="min-h-screen">
+      <Navbar />
+      <SignalHero />
     </main>
   )
 }

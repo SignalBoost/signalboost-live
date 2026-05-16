4a9f212e965c61b6b20b6309b62f21480eafebfa
SignalBoost <cadomos@gmail.com>
2026-05-16 01:28:41 -0600

    Add PodcastSection to the home page layout

--- saas/app/page.tsx
+++ saas/app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from '@/components/Navbar'
 import SignalHero from '@/components/SignalHero'
+import PodcastSection from '@/components/PodcastSection'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main style={{ minHeight: '100vh', background: '#0a0a0f' }}>
       <Navbar />
       <SignalHero />
+      <div style={{ borderTop: '1px solid rgba(255,255,255,0.05)' }}>
+        <PodcastSection />
+      </div>
     </main>
   )
 }

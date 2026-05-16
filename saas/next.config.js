b2d0e2cda8d7a57894c86d083fdf84005dd442bb
SignalBoost <cadomos@gmail.com>
2026-05-15 20:14:32 -0600

    Update next.config.js

--- saas/next.config.js
+++ saas/next.config.js
@@ -1,3 +1,8 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['signalboostapp.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig

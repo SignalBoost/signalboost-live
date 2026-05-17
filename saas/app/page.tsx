2dcfe5549670a38a987535c6849086a4f1dfd5a1
SignalBoost <cadomos@gmail.com>
2026-05-16 23:06:05 -0600

    pa

---
+++ saas/app/page.tsx
@@ -0,0 +1,13 @@
+import SignalHero from '@/components/SignalHero'
+import PodcastSection from '@/components/PodcastSection'
+
+export default function Home() {
+  return (
+    <main style={{ minHeight: '100vh', background: '#0a0a0f' }}>
+      <SignalHero />
+      <div style={{ borderTop: '1px solid rgba(255,255,255,0.05)' }}>
+        <PodcastSection />
+      </div>
+    </main>
+  )
+}

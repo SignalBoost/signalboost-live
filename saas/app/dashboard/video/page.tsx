ff53019ac4c124451e0f1e71c0b56fb42f0987ec
SignalBoost <cadomos@gmail.com>
2026-06-06 14:07:38 -0600

    Remove PreviewLimitGuard from VideoPage component

--- saas/app/dashboard/video/page.tsx
+++ saas/app/dashboard/video/page.tsx
@@ -1,11 +1,5 @@
-import PreviewLimitGuard from '@/components/video/PreviewLimitGuard'
 import VideoEditor from '@/components/video/VideoEditor'
 
 export default function VideoPage() {
-  return (
-    <>
-      <PreviewLimitGuard limit={3} enabled />
-      <VideoEditor />
-    </>
-  )
+  return <VideoEditor />
 }

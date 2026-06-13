75e7a387f6562ff0b6cb27f93294851fe81093ce
SignalBoost <cadomos@gmail.com>
2026-06-13 11:10:16 -0600

    Add index.ts for vault component exports
    Created an index file to export vault components and their props.

---
+++ saas/components/hub/vault/index.ts
@@ -0,0 +1,11 @@
+// saas/components/hub/vault/index.ts
+// Export all vault components.
+
+export { default as UnlockScreen } from './UnlockScreen'
+export type { UnlockScreenProps } from './UnlockScreen'
+
+export { default as VaultSecretsGrid } from './VaultSecretsGrid'
+export type { VaultSecretsGridProps } from './VaultSecretsGrid'
+
+export { default as VaultAuditLog } from './VaultAuditLog'
+export type { VaultAuditLogProps } from './VaultAuditLog'

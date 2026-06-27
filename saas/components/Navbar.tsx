19500db222adcab57155e9e8b6ab69ccdb8c63e8
SignalBoost <cadomos@gmail.com>
2026-06-27 09:30:52 -0600

    Expose full public product window in navbar

--- saas/components/Navbar.tsx
+++ saas/components/Navbar.tsx
@@ -1,1 +1,13 @@
-export { default } from './PremiumCustomerNavbarV2'
+'use client'
+
+import PremiumCustomerNavbarV2 from './PremiumCustomerNavbarV2'
+import PublicServiceWindowNav from './PublicServiceWindowNav'
+
+export default function Navbar() {
+  return (
+    <>
+      <PremiumCustomerNavbarV2 />
+      <PublicServiceWindowNav />
+    </>
+  )
+}

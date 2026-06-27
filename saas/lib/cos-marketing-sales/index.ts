7ab1272a67afaea28b4c7c14b3d255f16b9497b0
SignalBoost <cadomos@gmail.com>
2026-06-27 01:00:19 -0600

    Add COS marketing sales module index

---
+++ saas/lib/cos-marketing-sales/index.ts
@@ -0,0 +1,9 @@
+// saas/lib/cos-marketing-sales/index.ts
+// Public module exports for the portable COS Marketing + Sales Engine.
+
+export * from './types'
+export * from './schema'
+export * from './contactLimiter'
+export * from './salesOutreachManager'
+export * from './audioPodcastPipeline'
+export * from './printDeskPayloadMapper'

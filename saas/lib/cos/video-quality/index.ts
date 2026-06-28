802b8a8500e65815e8429be2733d44b3ffb149e9
SignalBoost <cadomos@gmail.com>
2026-06-28 12:08:22 -0600

    Export COS video quality harness

---
+++ saas/lib/cos/video-quality/index.ts
@@ -0,0 +1,2 @@
+export { buildVideoQualityComparison, scoreVideoCandidate } from './scoring'
+export type { VideoQualityCandidate, VideoQualityComparison, VideoQualityFeature, VideoQualityScore } from './types'

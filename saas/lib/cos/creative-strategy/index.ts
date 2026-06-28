6ca7e8952281ca3b8ca65d2050af50637d95bbe9
SignalBoost <cadomos@gmail.com>
2026-06-28 11:07:45 -0600

    Export COS creative strategy module

---
+++ saas/lib/cos/creative-strategy/index.ts
@@ -0,0 +1,2 @@
+export { buildCosHeroStrategy, defaultCosHeroStrategyInput } from './hero'
+export type { CosCreativeLanguage, CosHeroArchetype, CosHeroStrategy, CosHeroStrategyInput, CosMonetizationPath, CosTrafficGoal } from './types'

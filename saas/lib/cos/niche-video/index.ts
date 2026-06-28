2f8bb0beca09f744a9ef3312ccaff24632507b92
SignalBoost <cadomos@gmail.com>
2026-06-28 02:01:03 -0600

    Export COS niche video strategy

---
+++ saas/lib/cos/niche-video/index.ts
@@ -0,0 +1,2 @@
+export { buildNicheVideoConcept, defaultSignalBoostNicheVideoInput } from './strategy'
+export type { NicheVideoConcept, NicheVideoObjective, NicheVideoStrategyInput } from './types'

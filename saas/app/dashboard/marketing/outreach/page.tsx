f462a43a84dde9ec8db638ac188bb49be80c12a6
SignalBoost <cadomos@gmail.com>
2026-07-05 23:49:55 -0600

    Redirect old press channel outreach links

--- saas/app/dashboard/marketing/outreach/page.tsx
+++ saas/app/dashboard/marketing/outreach/page.tsx
@@ -1,5 +1,15 @@
+import { redirect } from 'next/navigation'
 import OutreachHubPage from '../../outreach/page'
 
-export default function MarketingOutreachPage() {
+const OLD_PRESS_CHANNELS = new Set(['online-newspapers', 'print-newspapers', 'trade-press'])
+
+type PageProps = {
+  searchParams?: Promise<{ channel?: string | string[] }> | { channel?: string | string[] }
+}
+
+export default async function MarketingOutreachPage({ searchParams }: PageProps) {
+  const params = await searchParams
+  const rawChannel = Array.isArray(params?.channel) ? params.channel[0] : params?.channel
+  if (rawChannel && OLD_PRESS_CHANNELS.has(rawChannel)) redirect('/dashboard/marketing/press-print')
   return <OutreachHubPage />
 }

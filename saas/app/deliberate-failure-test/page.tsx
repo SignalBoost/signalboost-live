9e96d0ebe44b809cb2469f04037039c41ed3fee0
SignalBoost <cadomos@gmail.com>
2026-07-28 13:46:21 -0600

    Create page.tsx

---
+++ saas/app/deliberate-failure-test/page.tsx
@@ -0,0 +1,4 @@
+// saas/app/deliberate-failure-test/page.tsx
+export default function DeliberateFailureTest() {
+  return <main><h1>Deliberate build failure for supervisor observation test</h1></main>
+}

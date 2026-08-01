7add29a2da03fbd0286be45decca04aa022e60d4
SignalBoost <cadomos@gmail.com>
2026-08-01 16:41:58 -0600

    Add socialOutreachConnectorManifest to exports

--- saas/lib/portable-products/manifests/index.ts
+++ saas/lib/portable-products/manifests/index.ts
@@ -1,3 +1,4 @@
+// saas/lib/portable-products/manifests/index.ts
 import { agentOperationsPlatformManifest } from './agentOperationsPlatform.ts'
 import { browserAgentEcosystemManifest } from './browserAgentEcosystem.ts'
 import { campaignStudioManifest } from './campaignStudio.ts'
@@ -8,12 +9,14 @@ import { portableChiefOfStaffManifest } from './portableChiefOfStaff.ts'
 import { pressMediaManifest } from './pressMedia.ts'
 import { providerHubManifest } from './providerHub.ts'
 import { selfHealingSupervisorManifest } from './selfHealingSupervisor.ts'
+import { socialOutreachConnectorManifest } from './socialOutreachConnector.ts'
 import { videoMakerManifest } from './videoMaker.ts'
 
-export { agentOperationsPlatformManifest, browserAgentEcosystemManifest, campaignStudioManifest, controlCenterManifest, integrationsHubManifest, marketingSalesManifest, portableChiefOfStaffManifest, pressMediaManifest, providerHubManifest, selfHealingSupervisorManifest, videoMakerManifest }
+export { agentOperationsPlatformManifest, browserAgentEcosystemManifest, campaignStudioManifest, controlCenterManifest, integrationsHubManifest, marketingSalesManifest, portableChiefOfStaffManifest, pressMediaManifest, providerHubManifest, selfHealingSupervisorManifest, socialOutreachConnectorManifest, videoMakerManifest }
 
 export const portableProductManifests = Object.freeze([
   providerHubManifest, campaignStudioManifest, integrationsHubManifest, videoMakerManifest, controlCenterManifest,
   marketingSalesManifest, pressMediaManifest, portableChiefOfStaffManifest,
   browserAgentEcosystemManifest, agentOperationsPlatformManifest, selfHealingSupervisorManifest,
+  socialOutreachConnectorManifest,
 ])

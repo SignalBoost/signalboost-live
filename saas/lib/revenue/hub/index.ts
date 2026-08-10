f86feb8cf41ae1af557e3972f099b4fc3979ea0e
SignalBoost <cadomos@gmail.com>
2026-08-09 20:46:31 -0600

    feat(revenue): export revenue intelligence hub

---
+++ saas/lib/revenue/hub/index.ts
@@ -0,0 +1,11 @@
+export { buildRevenueIntelligence } from './revenue-hub.ts'
+export { buildRevenueLearningPayload, persistRevenueIntelligence } from './memory.ts'
+export { buildRevenueOptimizationSignals, type RevenueOptimizationSignal } from './signals.ts'
+export type {
+  RevenueAttribution,
+  RevenueCurrencyMetrics,
+  RevenueForecast,
+  RevenueFunnel,
+  RevenueHubInput,
+  RevenueIntelligenceSnapshot,
+} from './types.ts'
